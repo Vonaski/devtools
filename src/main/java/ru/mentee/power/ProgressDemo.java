5d2ff622f3fecc7a92d2182a396ca71ab931a5b6
refle <reflex.11.04.1999@gmail.com>
2026-07-26 22:22:27 +0500

    fix

--- src/main/java/ru/mentee/power/ProgressDemo.java
+++ src/main/java/ru/mentee/power/ProgressDemo.java
@@ -2,11 +2,9 @@ package ru.mentee.power;
 
 import ru.mentee.power.devtools.student.Student;
 import ru.mentee.power.devtools.student.StudentList;
-import ru.mentee.power.progress.Mentee;
-import ru.mentee.power.progress.ProgressTracker;
 
 public class ProgressDemo {
-  static void main() {
+    static void main() {
 //    Mentee mentee1 = new Mentee("Ivan", "Moscow", "Offer", 5, 12);
 //    Mentee mentee2 = new Mentee("Mariya", "Moscow", "Offer", 8, 12);
 //    Mentee mentee3 = new Mentee("Petr", "Moscow", "Offer", 12, 12);
@@ -14,8 +12,8 @@ public class ProgressDemo {
 //    Mentee[] mentees = new Mentee[]{mentee1, mentee2, mentee3};
 //
 //    ProgressTracker.calculateTotalProgress(mentees);
-    Student student = new Student("Ilyas", "Tashkent");
-    StudentList studentList = new StudentList();
-    studentList.addStudent(student);
-  }
+        Student student = new Student("Ilyas", "Tashkent");
+        StudentList studentList = new StudentList();
+        studentList.addStudent(student);
+    }
 }
